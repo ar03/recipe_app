be37bb290b316200e313d708d5f4a14cef06de06
Geralt of Rivia <alex@AlexH.local>
2019-06-02 21:40:20 -0500

    inital commit

---
+++ babel.config.js
@@ -0,0 +1,15 @@
+/*module.exports = {
+  "presets": [
+    [
+      "@babel/preset-env",
+      {
+        "targets": {
+          "node": "10.16.0"
+        }
+      }
+    ]
+  ],
+  "plugins": [
+    "@babel/plugin-proposal-class-properties"
+  ]}
+  */
