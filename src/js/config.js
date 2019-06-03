672c192696e583ac817f2bddc0e4771b1d6c0031
Geralt of Rivia <alex@AlexH.local>
2019-06-03 06:01:00 -0500

    add recipe model

---
+++ src/js/config.js
@@ -0,0 +1,2 @@
+export const proxy = 'https://cors-anywhere.herokuapp.com/';
+export const key = '17fa85c901295997a0841b70e361f378';
--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 import Search from './models/Search';
+import Recipe from './models/Recipe';
 import * as searchView from './views/searchView'
 import { elements, renderLoader, clearLoader } from './views/base';
 
@@ -10,6 +11,7 @@ import { elements, renderLoader, clearLoader } from './views/base';
  */
 const state = {}
 
+/* Search Controller */
 const controlSearch = async () => {
     // 1) Get query from view
     const query = searchView.getInput();//ToDo
@@ -45,7 +47,28 @@ elements.searchResPages.addEventListener('click', e => {
     }
 });
 
+/* Recipe Controller */
+const controlRecipe = async () => {
+    // Get ID from url
+    const id = window.location.hash.replace('#', '');
+    console.log(id);
 
+    if (id) {
+        // Prepare UI for changes
+
+        // Create new recipe object
+        state.recipe = new Recipe(id);
+        // Get recipe data
+        await state.recipe.getRecipe();
+        // Calc servings and time
+        state.recipe.calcTime();
+        state.recipe.calcServings();
+        // Render recipe
+        console.log(state.recipe);
+    }
+};
+
+['hashchange', 'load'].forEach(event => window.addEventListener(event, controlRecipe));
 
 
 // http://food2fork.com/api/search
